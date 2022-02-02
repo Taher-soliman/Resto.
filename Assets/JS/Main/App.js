867c8d1c4afe05956386495ee169b3ede9f4e552
Taher Soliman <taherelbadawy38@gmail.com>
2022-02-03 00:21:49 +0200

    Full Nav bar

--- Assets/JS/Main/App.js
+++ Assets/JS/Main/App.js
@@ -1,1 +1,25 @@
-console.log("aaaaa");
+// Toggle Nav Menu
+const menuBtn = document.querySelector("#menu-bars");
+const navbar = document.querySelector(".navbar-sections");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("clicked");
+  navbar.classList.toggle("active-nav");
+  if (menuBtn.classList.contains("clicked")) {
+    menuBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  } else {
+    menuBtn.innerHTML = `<i class="fas fa-bars"></i>`;
+  }
+});
+
+// Show Search Form
+const closeSearch = document.querySelector(".close");
+const searchIcon = document.querySelector(".search");
+
+// Open Search
+searchIcon.addEventListener("click", () => {
+  document.querySelector("#search-form").classList.toggle("active");
+});
+// Close Search
+closeSearch.addEventListener("click", () => {
+  document.querySelector("#search-form").classList.remove("active");
+});
